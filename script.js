bcf9302d14458dc7d28bd7d79dbf491a552b2a09
lucasdiego33 <lukinha_candido@hotmail.com>
2024-03-05 20:32:33 -0300

    Definindo valor inicial,como menor que valor final para inicio do sorteio

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ function generateNumber(){
     const min = Math.ceil(document.querySelector(".meu-input1").value)
     const max =  Math.floor(document.querySelector(".meu-input2").value)
     
-    
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+
+    if(min >= max){
+        alert("o valor minimo tem que ser menor que o valor maximo")
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
     alert(result)
+    }
+   
+    
+
+    
     }
